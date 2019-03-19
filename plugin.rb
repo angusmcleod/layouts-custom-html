298926128a4820b62ec99e9d0a0fbaa13b0942a2
Angus McLeod <angus@mcleod.org.au>
2019-03-19 12:42:40 +1100

    Preload category custom fields

--- plugin.rb
+++ plugin.rb
@@ -8,5 +8,6 @@ DiscourseEvent.on(:layouts_ready) do
 end
 
 after_initialize do
+  Site.preloaded_category_custom_fields << 'layouts_custom_html' if Site.respond_to? :preloaded_category_custom_fields
   add_to_serializer(:basic_category, :layouts_custom_html) { object.custom_fields['layouts_custom_html'] }
 end
